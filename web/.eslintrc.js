23b23e02a557c88219407174e8e0b7e03c1cfdf9
kawashima <pyjama.kud@gmail.com>
2022-06-20 23:25:32 +0900

    chore: add eslint

---
+++ web/.eslintrc.js
@@ -0,0 +1,30 @@
+/** @type {import('eslint/lib/shared/types').ConfigData} */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".jsx", ".tsx"],
+      },
+    ],
+  },
+};
